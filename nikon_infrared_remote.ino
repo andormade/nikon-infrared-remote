4d929143bb46ad1c5fd4731dd406c3138e891737
andormade <hngrhorace@gmail.com>
2020-10-09 20:59:24 +0200

    initial commit

---
+++ nikon_infrared_remote.ino
@@ -0,0 +1,25 @@
+#define IR_LED_PIN 5
+
+void setup() {
+  pinMode(IR_LED_PIN, OUTPUT);
+  doTheBurst(76);
+  delay(27);
+  delayMicroseconds(810);
+  doTheBurst(16);
+  delayMicroseconds(1540);
+  doTheBurst(16);
+  delayMicroseconds(3545);
+  doTheBurst(16);
+}
+
+void doTheBurst(int count) {
+  for (int i = 0; i < count; i++) {
+    digitalWrite(IR_LED_PIN, HIGH);
+    delayMicroseconds(7);
+    digitalWrite(IR_LED_PIN, LOW);
+    delayMicroseconds(7);
+  }
+}
+
+void loop() {
+}
